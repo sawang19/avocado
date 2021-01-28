fc56f37a28b136adda5d508432649e393ab3b31b
Sa Wang <wangsa19960109@gmail.com>
2021-01-28 12:40:00 +0800

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform playerTransform;
+    private float oneKeySize = 2.5f;
+    private float twoKeySize = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+
+        if(Player.keys == 1)
+        {
+            Camera.main.orthographicSize = oneKeySize;
+        }
+        if (Player.keys == 2)
+        {
+            Camera.main.orthographicSize = twoKeySize;
+        }
+        
+        transform.position = temp;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public static int keys = 0;
+    public Text keyAmount;
+    public Text youwin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+        	transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+        	transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+        	transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+        	transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Keys")
+        {
+            keys++;
+            keyAmount.text = "Keys: " + keys;
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag == "Enemies")
+        {
+            keys = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (collision.gameObject.tag == "Door" && keys == 4)
+        {
+            youwin.text = "YOU WIN!";
+        }
+
+        if (collision.gameObject.tag == "Walls")
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
+        }
+    }
+}
