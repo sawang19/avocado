ff38f1b4e2407409f288d5b80418cf65e5e05210
Sa Wang <wangsa19960109@gmail.com>
2021-04-15 09:03:43 +0800

    backbutton

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -6,8 +6,31 @@ using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
-    public BoolValue Level2;
-    public BoolValue Level3;
+    //public BoolValue Level2;
+    //public BoolValue Level3;
+    private GameObject level1;
+    private GameObject level2;
+    private GameObject level3;
+    private Color32 originalColor;
+    void Start()
+    {
+        level1 = GameObject.Find("Level1");
+        level2 = GameObject.Find("Level2");
+        level3 = GameObject.Find("Level3");
+        originalColor = level1.GetComponent<Image>().color;
+    }
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("level2") == 1)
+        {
+            level2.GetComponent<Image>().color = originalColor;
+        }
+        if (PlayerPrefs.GetInt("level3") == 1)
+        {
+            level3.GetComponent<Image>().color = originalColor;
+        }
+    }
+
     public void LoadLevel1()
     {
         PlayerPrefs.SetInt("levels", 1);
@@ -30,6 +53,11 @@ public class LevelSelect : MonoBehaviour
             SceneManager.LoadScene("Loading3");
 
         }
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MenuScene");
     }
 
 }
