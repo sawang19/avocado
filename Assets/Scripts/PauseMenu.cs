a4b0e05ee3a2fe47c8b65c3b64951f0bd8611e89
Sa Wang <wangsa19960109@gmail.com>
2021-04-25 19:48:50 +0800

    finalversion2

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class PauseMenu : MonoBehaviour
 {
     public GameObject PauseUI, Pausemenu, PauseButton;
-    
+    public Slider mSlider;
+
+    void Update()
+    {
+        mSlider.value = PlayerPrefs.GetFloat("Volume");
+    }
+
     public void Pause()
     {
         PauseUI.SetActive(true);
